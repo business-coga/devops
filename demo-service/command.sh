82938fdb109881d8bad21167b3e1f7ff2a4cc2b1
business-coga <66541575+business-coga@users.noreply.github.com>
2020-12-15 10:26:01 +0700

    add

--- demo-service/command.sh
+++ demo-service/command.sh
@@ -1,10 +1,6 @@
-docker build --no-cache -t hieuhv2/node-app .
+docker build -t hieuhv2/node-app .
 docker push hieuhv2/node-app
-kubectl delete deployment node-app
-
-kubectl apply -f devops/deployment.mongo.yaml
-kubectl apply -f devops/service.mongo.yaml
-kubectl apply -f devops/deployment.app.yaml
-kubectl apply -f devops/service.app.yaml
-
-https://kubernetes.io/vi/docs/reference/kubectl/cheatsheet/
+kubectl apply -f devops/deployment-mongo.yaml
+kubectl apply -f devops/service-mongo.yaml
+kubectl apply -f devops/deployment-nodejs.yaml
+kubectl apply -f devops/service-nodejs.yaml
